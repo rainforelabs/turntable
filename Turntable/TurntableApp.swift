cc4cbcdedd85af5b27a80b5aa59c6a5698ec5431
adityarohman <aditya@tribel.dev>
2026-08-05 11:59:00 +0800

    Initial Commit

---
+++ Turntable/TurntableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurntableApp.swift
+//  Turntable
+//
+//  Created by Aditya Rohman on 05/08/26.
+//
+
+import SwiftUI
+
+@main
+struct TurntableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
