2ea4f78c22c5bc1a4a755d76229e062e3953152b
adityarohman <aditya@tribel.dev>
2026-08-05 12:18:24 +0800

    chore: project setup

---
+++ Turntable/Features/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Turntable
+//
+//  Created by Aditya Rohman on 05/08/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+
+#Preview {
+    HomeView()
+}
